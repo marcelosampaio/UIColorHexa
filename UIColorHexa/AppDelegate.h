5ae4dd2eeba3ef852fdec3e81f1a80a1aedf6afb
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-06-01 10:43:01 -0300

    Initial Commit

---
+++ UIColorHexa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIColorHexa
+//
+//  Created by Cast on 6/1/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIColorHexa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIColorHexa
+//
+//  Created by Cast on 6/1/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
